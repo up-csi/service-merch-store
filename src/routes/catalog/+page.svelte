c0ffb9a83da070cc7b0e610ae86ba9700979e4f1
Kolleen <kolleengeri.aguilar11@gmail.com>
2026-03-28 20:26:23 +0800

    feat(catalog/+page): integrate advance search into catalog page

--- src/routes/catalog/+page.svelte
+++ src/routes/catalog/+page.svelte
@@ -1,1 +1,12 @@
-<h1>This is the Catalog Page</h1>
+<script lang="ts">
+    import AdvanceSearch from "$lib/components/AdvanceSearch/advance-search.svelte";
+</script>
+
+<div class="flex px-5">
+    <!-- Sording Sidebar -->
+    <div class="w-80 flex-shrink-0">
+        <AdvanceSearch />
+    </div>
+
+    <!-- Catalog Grid -->
+</div>
